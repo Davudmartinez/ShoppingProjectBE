e84867771642a26315016a2d3cee982e9fec3a7a
vocuongthinh2@gmail.com <vocuongthinh2@gmail.com>
2025-03-06 23:44:32 +0700

    hoc ly thuyet r code

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import express from 'express' //import express vào dự án
 const app = express() //dùng express tạo 1 server
-const port = 3000 //server sẽ chạy trên cổng port 3000
+const PORT = 3000 //server sẽ chạy trên cổng port 3000
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.listen(port, () => {
-  console.log(`Server BE đang chạy trên PORT:  ${port}`)
+app.listen(PORT, () => {
+  console.log('Server BE đang chạy trên PORT: ' + PORT)
 })
-
-//anh thinh dep trai qua ta
-//
